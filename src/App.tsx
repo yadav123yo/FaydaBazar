021c363d9db1feafb27c9cf50130ce9e57a13f5c
kedar05121998 <101327577+kedar05121998@users.noreply.github.com>
2022-12-13 14:49:52 +0530

    inital commit-Boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import "./App.css";
+import AllRoutes from "./All-Routes/AllRoutes";
+
+/*
+Dont make any changes to this file
+*/
+
+function App() {
+  return (
+    <>
+      <AllRoutes />
+    </>
+  );
+}
+
+export default App;
