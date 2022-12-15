73784893d53de5b70bd711b09ae2264aa4bc36fa
saurabh solanki <solankisourabh25@gmail.com>
2022-12-15 19:24:54 +0530

    day3

--- src/Pages/Product/AllProduct.js
+++ src/Pages/Product/AllProduct.js
@@ -1,8 +1,9 @@
 import "./AllProduct.css";
-import { Box, Button, Flex, Text, useToast } from '@chakra-ui/react';
+import { Box, Button, Flex, Spacer, Text, useToast } from '@chakra-ui/react';
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
 import { Link } from 'react-router-dom';
+import SideBar from "./SideBar";
 
 const AllProduct = () => {
   const [data, setData] = useState([])
@@ -49,39 +50,47 @@ useEffect(() => {
                   <Button className='sortbtn'>Popularity</Button>
                   <Button className='sortbtn' onClick={handlesortA}>High to Low</Button>
                   <Button className='sortbtn' onClick={handlesortB}>Low to High</Button>
-                  <Button className='sortbtn'>Discount</Button>
               </Box>
           </Box>
         </div>
 
+        <div id="mainDiv">
 
-        {/* This is productlist div */}
-        <div id="productList">
+            {/* side Bar */}
+
+            <Box className='mi1'> <SideBar /></Box>
+
+                {/* This is productlist div */}
+                <div id="productList">
         {
 
           data.map((e) => (
              <div id='singleProduct' key={e.id}>
               <img src={e.image} alt="" />
-              <h1>{e.title}</h1>
+              <Text fontSize='xl'>{e.title}</Text>
 
                 <div id="productPrice">
                   <div id="productMrp">
-                    <Text color="#6f7284" fontSize="xs" as="s">${e.original_price}{" "}</Text>
-                    <Text color="#6f7284" fontSize="s">${" "} {e.offer_price}</Text>
+                    <Text fontSize="xs" as="s" color='tomato'>{e.original_price}{" "}</Text>
+                    <Text color="black" fontSize="s" as='b'>₹{" "} {e.offer_price}</Text>
                   </div>
                 </div>
                 
-                <Text>Free Shipping</Text>
+                <Text color="teal" fontSize="xs">Free Shipping</Text>
 
                 <div id="productButton">
-                  <Button className="btn1">SEE DETAILS</Button>
-                  <Button className="btn2">ADD TO CART</Button>
+                  <Button colorScheme="white" color="black" variant='outline' className="btn1">SEE DETAILS</Button>
+                  <Spacer />
+                  <Button colorScheme="facebook" className="btn2">ADD TO CART</Button>
                 </div>
 
             </div>
             ))
         }
         </div>
+        </div>
+
+
 
                     <Flex justifyContent="center" gap="30px" marginTop="30px">
                         <Button disabled={page === 1} colorScheme='teal' variant='solid' onClick={() => setpage((prev) => prev - 1)}>Prev</Button>
