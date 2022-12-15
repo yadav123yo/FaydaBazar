b43a2237237fded9df378f021bf14d1dbd7f68c8
Yogesh Yadav <yy072156@gmail.com>
2022-12-15 16:48:05 +0530

    signup

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,9 +1,150 @@
-import React from 'react'
+import {
+  useToast,
+  Input,
+  Box,
+  Grid,
+  Img,
+  Heading,
+  Stack,
+  Button,
+  InputGroup,
+  InputLeftElement,
+} from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { EmailIcon, LockIcon } from "@chakra-ui/icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { authRegister } from "../../Redux/auth/action";
+import { useNavigate } from "react-router-dom";
+import { AUTH_REGISTER_RESET } from "../../Redux/auth/actionTypes";
 
-const Signup = () => {
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+};
+function Signup() {
+  const toast = useToast();
+  const navigate = useNavigate();
+  const [formData, setFormData] = React.useState(initialState);
+  const authState = useSelector((state) => state.auth.userRegister);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (authState.message === "User already exists") {
+      toast({
+        title: authState.message,
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
+      });
+      dispatch({ type: AUTH_REGISTER_RESET });
+    }
+    if (authState.message === "User Registered Successfully") {
+      toast({
+        title: authState.message,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
+      });
+
+      dispatch({ type: AUTH_REGISTER_RESET });
+
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000);
+    }
+  }, [dispatch, navigate, authState.error, authState.message, toast]);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(formData);
+    dispatch(authRegister(formData));
+  };
   return (
-    <div>Signup</div>
-  )
+    <Grid
+      w={{ base: "70%", md: "90%" }}
+      m="30px auto"
+      gap={8}
+      templateColumns={{ base: "repeat(1,1fr)", md: "repeat(2,1fr)" }}
+    >
+      <Box shadow={{ base: "md", md: "none" }} borderRadius="20px" p={4}>
+        <Heading textAlign={"center"}>Sign up..</Heading>
+        <Stack py={8} gap={4} w={{ base: "85%", md: "70%" }} m="auto">
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<FontAwesomeIcon icon={faUser} />}
+            />
+            <Input
+              type={"text"}
+              onChange={handleChange}
+              name="name"
+              placeholder="Username"
+              value={formData.name}
+            />
+          </InputGroup>
+          <InputGroup>
+            {" "}
+            <InputLeftElement
+              pointerEvents="none"
+              children={<EmailIcon color="gray.600" />}
+            />
+            <Input
+              type="email"
+              onChange={handleChange}
+              name="email"
+              placeholder="Email"
+              value={formData.email}
+              required={true}
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<LockIcon color="gray.600" />}
+            />
+            <Input
+              type={"password"}
+              onChange={handleChange}
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+            />
+          </InputGroup>
+
+          <Button
+            bg={"rgb(88, 88, 88)"}
+            onClick={handleSubmit}
+            _hover={{ bg: "black" }}
+            color={"white"}
+            fontSize="18px"
+            w={"100%"}
+          >
+            {authState.loading ? "Registering..." : "Register"}
+          </Button>
+          <Button onClick={() => navigate("/login")}>
+            Already a User ? Login
+          </Button>
+        </Stack>
+      </Box>
+      <Box display={{ base: "none", md: "inherit" }}>
+        <Img
+          w={"100%"}
+          h={"100%"}
+          borderRadius={50}
+          objectFit="contain"
+          src="https://chameleoncollective.com/wp-content/uploads/2018/04/e-commerce-blog-post-scaled.jpg"
+        />
+      </Box>
+    </Grid>
+  );
 }
 
-export default Signup
+export default Signup;
---
+++ src/Redux/products/reducer.js
@@ -0,0 +1,132 @@
+import {
+    ADD_PRODUCT_FAILURE,
+    ADD_PRODUCT_REQUEST,
+    ADD_PRODUCT_SUCCESS,
+    DELETE_PRODUCT_FAILURE,
+    DELETE_PRODUCT_REQUEST,
+    DELETE_PRODUCT_SUCCESS,
+    GET_ALL_PRODUCTS_FAILURE,
+    GET_ALL_PRODUCTS_REQUEST,
+    GET_ALL_PRODUCTS_SUCCESS,
+    GET_SINGLE_PRODUCT_FAILURE,
+    GET_SINGLE_PRODUCT_REQUEST,
+    GET_SINGLE_PRODUCT_SUCCESS,
+    UPDATE_PRODUCT_FAILURE,
+    UPDATE_PRODUCT_REQUEST,
+    UPDATE_PRODUCT_SUCCESS,
+  } from "./actionTypes";
+  
+  const initialState = {
+    AllProducts: { loading: false, error: false },
+    Product: { loading: false, error: false },
+    AddProduct: { loading: false, error: false },
+    UpdateProduct: { loading: false, error: false },
+    DeleteProduct: { loading: false, error: false },
+    data: [],
+    singleData: {},
+  };
+  
+  export default function productsReducer(
+    state = initialState,
+    { type, payload }
+  ) {
+    switch (type) {
+      case GET_ALL_PRODUCTS_REQUEST:
+        return {
+          ...state,
+          AllProducts: { loading: true, error: false },
+        };
+      case GET_ALL_PRODUCTS_SUCCESS:
+        return {
+          ...state,
+          AllProducts: { loading: false, error: false },
+          data: payload.products,
+        };
+  
+      case GET_ALL_PRODUCTS_FAILURE:
+        return {
+          ...state,
+          AllProducts: { loading: false, error: true },
+        };
+  
+      case GET_SINGLE_PRODUCT_REQUEST:
+        return {
+          ...state,
+          Product: { loading: true, error: false },
+        };
+      case GET_SINGLE_PRODUCT_SUCCESS:
+        return {
+          ...state,
+          Product: {
+            loading: false,
+            error: false,
+          },
+          singleData: payload,
+        };
+  
+      case GET_SINGLE_PRODUCT_FAILURE:
+        return {
+          ...state,
+          Product: { loading: false, error: true },
+        };
+  
+      case ADD_PRODUCT_REQUEST:
+        return {
+          ...state,
+          AddProduct: { loading: true, error: false },
+        };
+      case ADD_PRODUCT_SUCCESS:
+        return {
+          ...state,
+          AddProduct: { loading: false, error: false },
+          data: [...state.data, payload],
+        };
+  
+      case ADD_PRODUCT_FAILURE:
+        return {
+          ...state,
+          AddProduct: { loading: false, error: true },
+        };
+  
+      case UPDATE_PRODUCT_REQUEST:
+        return {
+          ...state,
+          UpdateProduct: { loading: true, error: false },
+        };
+      case UPDATE_PRODUCT_SUCCESS:
+        return {
+          ...state,
+          UpdateProduct: { loading: false, error: false },
+          data: state.data.map((item) =>
+            item._id === payload._id ? payload : item
+          ),
+        };
+  
+      case UPDATE_PRODUCT_FAILURE:
+        return {
+          ...state,
+          UpdateProduct: { loading: false, error: true },
+        };
+  
+      case DELETE_PRODUCT_REQUEST:
+        return {
+          ...state,
+          DeleteProduct: { loading: true, error: false },
+        };
+      case DELETE_PRODUCT_SUCCESS:
+        return {
+          ...state,
+          DeleteProduct: { loading: false, error: false },
+          data: state.data.filter((item) => item._id !== payload),
+        };
+  
+      case DELETE_PRODUCT_FAILURE:
+        return {
+          ...state,
+          DeleteProduct: { loading: false, error: true },
+        };
+  
+      default:
+        return state;
+    }
+  }
