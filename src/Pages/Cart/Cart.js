1024e8a8880f7d48093a11c81c7d8b76629d03b7
Yogesh Yadav <yy072156@gmail.com>
2022-12-16 19:10:02 +0530

    cart-delete

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -1,61 +1,9 @@
-import { Box, Flex, Heading, useToast } from "@chakra-ui/react";
-import "./Cart.css";
-// import Left from "../../Components/Cart/left/Left";
-// import Right from "../../Components/Cart/right/Right";
-import Left from "./Left/Left";
-import Right from "./Right/Right";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import React from 'react'
 
-function Cart() {
-  const auth = useSelector((store) => store.auth);
-  const toast = useToast();
-  // console.log(auth);
-  if (auth.data.isAuthenticated === false) {
-    toast({
-      title: "Login Error",
-      description: "Please login first to access cart",
-      status: "error",
-      duration: 3000,
-      isClosable: true,
-      position: "top",
-    });
-    toast({
-      title: "Redirecting",
-      description: "Redirecting to signup page",
-      status: "error",
-      duration: 3000,
-      isClosable: true,
-      position: "top",
-    });
-    return <Navigate to="/signup" />;
-  }
-
+const Cart = () => {
   return (
-    <Box
-      w={{ lg: "90%", md: "100%", base: "100%" }}
-      h="auto"
-      m="auto"
-      mt="20px"
-      mb="20px"
-      p="10px"
-    >
-      <Heading
-        fontSize={{ lg: "25px", md: "20px", base: "25px" }}
-        pb={{ lg: "20px", md: "20px", base: "10px" }}
-      >
-        My Basket
-      </Heading>
-      <Flex
-        gap={{ lg: "15px", md: "15px" }}
-        h="auto"
-        flexDirection={{ lg: "row", md: "row", base: "column-reverse" }}
-      >
-        <Left />
-        <Right />
-      </Flex>
-    </Box>
-  );
+    <div>Cart</div>
+  )
 }
 
-export default Cart;
+export default Cart
