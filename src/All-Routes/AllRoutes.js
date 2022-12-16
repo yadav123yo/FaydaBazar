6fe93be8322834b650a1b11a00e7717c1f6d88c8
Yogesh Yadav <yy072156@gmail.com>
2022-12-16 09:00:05 +0530

    routes-chenge

--- src/All-Routes/AllRoutes.js
+++ src/All-Routes/AllRoutes.js
@@ -8,6 +8,9 @@ import Login from '../Pages/Login/Login';
 import AllProduct from '../Pages/Product/AllProduct';
 import SingleProduct from '../Pages/Product/SingleProduct';
 import Signup from '../Pages/Signup/Signup';
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+
 
 /*
 Dont make any changes to this file
@@ -16,13 +19,13 @@ Dont make any changes to this file
 const AllRoutes = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/product" element={<AllProduct />} />
-            <Route path="/product/:id" element={<SingleProduct />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/" element={<><Navbar /> <Home />  <Footer /></>} />
+            <Route path="/login" element={<><Navbar /> <Login/> <Footer /></>} />
+            <Route path="/signup" element={ <><Navbar /> <Signup />  <Footer /></>} />
+            <Route path="/product" element={   <><Navbar /> <AllProduct />   <Footer /></>} />
+            <Route path="/product/:id" element={<><Navbar /><SingleProduct /> <Footer /></>} />
+            <Route path="/cart" element={ <><Navbar /><Cart />     <Footer /></>} />
+            <Route path="/checkout" element={  <><Navbar /><Checkout />  <Footer /></>} />
             <Route path="/admin/dashboard" element={<Dashboard />} />
             <Route path="/admin/adminForm" element={<AdminForm />} />
         </Routes>
