67135f9a45dbddfbfa07ca97133ff92f60b77fda
Yogesh Yadav <yy072156@gmail.com>
2022-12-18 13:02:08 +0530

    cart

--- src/Components/Cart/left/Left.jsx
+++ src/Components/Cart/left/Left.jsx
@@ -1,20 +1,20 @@
-import React from "react";
+// import React from "react";
 
-import { Box } from "@chakra-ui/react";
-import GetItShipped from "./GetItShipped";
+// import { Box } from "@chakra-ui/react";
+// import GetItShipped from "./GetItShipped";
 
-const Left = () => {
-  return (
-    <Box
-      id="left-container"
-      w={{ lg: "65%", md: "65%", base: "100%" }}
-      h={{ lg: "auto", md: "auto", base: "auto" }}
-      // border="1px solid green"
-    >
+// const Left = () => {
+//   return (
+//     <Box
+//       id="left-container"
+//       w={{ lg: "65%", md: "65%", base: "100%" }}
+//       h={{ lg: "auto", md: "auto", base: "auto" }}
+//       // border="1px solid green"
+//     >
     
-      <GetItShipped />
-    </Box>
-  );
-};
+//       <GetItShipped />
+//     </Box>
+//   );
+// };
 
-export default Left;
+// export default Left;
--- src/Components/Cart/right/Right.jsx
+++ src/Components/Cart/right/Right.jsx
@@ -1,81 +1,81 @@
-import { Box, Button, Divider, Flex, Stack, Text } from "@chakra-ui/react";
-import React from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+// import { Box, Button, Divider, Flex, Stack, Text } from "@chakra-ui/react";
+// import React from "react";
+// import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
 
-const Right = () => {
-  const navigate = useNavigate();
-  const { carts } = useSelector((state) => state.carts);
-  let total = carts.reduce(
-    (ac, ele) => ac + ele.productId.offer_price* ele.quantity,
-    0
-  );
+// const Right = () => {
+//   const navigate = useNavigate();
+//   const { carts } = useSelector((state) => state.carts);
+//   let total = carts.reduce(
+//     (ac, ele) => ac + ele.productId.offer_price* ele.quantity,
+//     0
+//   );
 
-  const checkout = () => {
-    navigate("/checkout");
-  };
+//   const checkout = () => {
+//     navigate("/checkout");
+//   };
 
-  return (
-    <Stack
-      w={{ lg: "35%", md: "35%", base: "100%" }}
-      border="1px solid grey"
-      borderRadius={"5px"}
-      h="100%"
-      p="10px 20px"
-      mb="10px"
-    >
-      <Flex w="100%" justify={"space-between"}>
-        <Text>Merchandise Subtotal</Text>
-        <Text fontWeight={"bold"}>₹ {total}</Text>
-      </Flex>
-      <Flex w="100%" justify={"space-between"}>
-        <Text>Shipping & Handling</Text>
-        <Text fontWeight={"bold"}>₹ 100</Text>
-      </Flex>
-      <Flex w="100%" justify={"space-between"}>
-        <Text>Tax (Inc.)</Text>
-        <Text fontWeight={"bold"}>{(total) / 100}</Text>
-      </Flex>
-      <Divider />
-      <Flex w="100%" justify={"space-between"}>
-        <Text>Estimated Total</Text>
-        <Text fontWeight={"bold"}>₹ {total  + 100}</Text>
-      </Flex>
-      <Box w="100%" fontSize="13px">
-        <Text>
-          or 4 payments of ₹ {(total ) / 4} with <strong>SBI</strong> or{" "}
-          <strong> afterpay</strong>
-        </Text>
-      </Box>
-      <Text fontSize={"12px"} color="grey">
-        Shipping & taxes calculated during checkout
-      </Text>
-      <Flex justify={"center"}>
-        {total == 0 ? (
-          <Button
-            w="70%"
-            background={"#cf112c"}
-            color="white"
-            borderRadius={"20px"}
-            _hover={{ background: " rgb(226, 3, 15)", color: "white" }}
-          >
-            Add Products to checkout
-          </Button>
-        ) : (
-          <Button
-            w="70%"
-            background={"#cf112c"}
-            color="white"
-            borderRadius={"20px"}
-            _hover={{ background: " rgb(226, 3, 15)", color: "white" }}
-            onClick={checkout}
-          >
-            Checkout
-          </Button>
-        )}
-      </Flex>
-    </Stack>
-  );
-};
+//   return (
+//     <Stack
+//       w={{ lg: "35%", md: "35%", base: "100%" }}
+//       border="1px solid grey"
+//       borderRadius={"5px"}
+//       h="100%"
+//       p="10px 20px"
+//       mb="10px"
+//     >
+//       <Flex w="100%" justify={"space-between"}>
+//         <Text>Merchandise Subtotal</Text>
+//         <Text fontWeight={"bold"}>₹ {total}</Text>
+//       </Flex>
+//       <Flex w="100%" justify={"space-between"}>
+//         <Text>Shipping & Handling</Text>
+//         <Text fontWeight={"bold"}>₹ 100</Text>
+//       </Flex>
+//       <Flex w="100%" justify={"space-between"}>
+//         <Text>Tax (Inc.)</Text>
+//         <Text fontWeight={"bold"}>{(total) / 100}</Text>
+//       </Flex>
+//       <Divider />
+//       <Flex w="100%" justify={"space-between"}>
+//         <Text>Estimated Total</Text>
+//         <Text fontWeight={"bold"}>₹ {total  + 100}</Text>
+//       </Flex>
+//       <Box w="100%" fontSize="13px">
+//         <Text>
+//           or 4 payments of ₹ {(total ) / 4} with <strong>SBI</strong> or{" "}
+//           <strong> afterpay</strong>
+//         </Text>
+//       </Box>
+//       <Text fontSize={"12px"} color="grey">
+//         Shipping & taxes calculated during checkout
+//       </Text>
+//       <Flex justify={"center"}>
+//         {total == 0 ? (
+//           <Button
+//             w="70%"
+//             background={"#cf112c"}
+//             color="white"
+//             borderRadius={"20px"}
+//             _hover={{ background: " rgb(226, 3, 15)", color: "white" }}
+//           >
+//             Add Products to checkout
+//           </Button>
+//         ) : (
+//           <Button
+//             w="70%"
+//             background={"#cf112c"}
+//             color="white"
+//             borderRadius={"20px"}
+//             _hover={{ background: " rgb(226, 3, 15)", color: "white" }}
+//             onClick={checkout}
+//           >
+//             Checkout
+//           </Button>
+//         )}
+//       </Flex>
+//     </Stack>
+//   );
+// };
 
-export default Right;
+// export default Right;
